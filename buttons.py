46c7137162e708ad32667923b104e991f307714d
Rodolfo <todocono@gmail.com>
2023-10-09 13:32:31 -0400

    Create buttons.py

---
+++ buttons.py
@@ -0,0 +1,38 @@
+# Part of of https://github.com/todocono/XAC-PACK
+
+# Clients: Children's Hospital Eastern Ontario
+# Organization: Tetra Society
+
+# Released under RG-M Pacifist License, which is basically a MIT modified license.
+# (original MIT license is available on http://www.opensource.org/licenses/mit-license).
+# The main amendment prevents the work under license or any other drived work from:
+# war purposes, or purposes related to death penalty.
+
+# Coded by Rodolfo Cossovich
+# Latest update: 2023-10-09
+
+
+def fnDOWN_down():
+    print('DOWN pressed')
+def fnDOWN_up():
+    print('DOWN released')
+def fnLEFT_down():
+    print('LEFt pressed!')
+def fnLEFT_up():
+    print('LEFt released!')  
+def fnRIGHT_down():
+    print('RIGHT pressed')
+def fnRIGHT_up():
+    print('RIGHT released')
+def fnUP_down():
+    print('UP pressed')
+def fnUP_up():
+    print('UP released')
+def fnCLICK_down():
+    print('CLICK pressed')
+def fnCLICK_up():
+    print('CLICK released')
+def fnRCLICK_down():
+    print('R-CLICK pressed')
+def fnRCLICK_up():
+    print('R-CLICK released')
