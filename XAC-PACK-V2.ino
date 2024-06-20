1f75cb52dd2d276d896cf43dfb9501d4bad4a258
Rodolfo <todocono@gmail.com>
2024-06-20 17:08:27 -0400

    Update XAC-PACK-V2.ino
    tested with joystick and head array

--- XAC-PACK-V2.ino
+++ XAC-PACK-V2.ino
@@ -43,6 +43,13 @@ void setup() {
   digitalWrite(8, HIGH);
 
   Joystick.begin(false);  // false indicates that sendState method call required (so we do all changes at once).
+  delay(1000);
+  Joystick.setXAxis(512);
+  Joystick.setYAxis(512);
+  Joystick.sendState();
+  //Serial.print("starting");
+  delay(1000);
+  //test2();
 }
 
 void loop() {
@@ -52,45 +59,50 @@ void loop() {
 // function that executes whenever data is received from master
 // this function is registered as an event, see setup()
 void receiveEvent(int howMany) {
-  if (howMany == 2) {
-    byte valueArray[2];
-    for (int i = 0; i < howMany; i++) {
-      valueArray[i] = Wire.read();
-    }
-    // while (1 < Wire.available()) {  // loop through all but the last
-    //   byte c = Wire.read();         // receive byte as a character
-    //   //Serial.print(c);              // print the character
-    // }
-    int x = valueArray[0] - 127;
-    int y = valueArray[1] - 127;
-    Serial.print(x);
-    Serial.print("\t");
-    Serial.println(y);
-    // Joystick.setXAxis(x);
-    // Joystick.setYAxis(y);
-    // Joystick.sendState();
+  //if (howMany == 2) {
+  byte valueArray[2];
+  while (2 < Wire.available()) {  // loop through all but the last
+    byte c = Wire.read();         // receive byte as a character
+    //Serial.print(c);              // print the character
   }
-
-  // int value = Wire.read();  // receive byte as an integer
-
-  // if (value != 0) {
-  //   // Serial.println(value, HEX);  // print the integer
-  //   int y = value & 0x0F;
-  //   int x = value & 0xF0;
-  //   //x = x << 4;
-  //   // Joystick.setXAxis(x * 4);
-  //   // Joystick.setYAxis(y * 4);
-  //   // Joystick.sendState();
-  //   x = x >> 4;
-  //   x = x * 64;
-  //   y = y * 64;
+  valueArray[1] = Wire.read();
+  valueArray[0] = Wire.read();
+  // while (1 < Wire.available()) {  // loop through all but the last
+  //   byte c = Wire.read();         // receive byte as a character
+  //   //Serial.print(c);              // print the character
+  // }
+  int x = (24 * (valueArray[0] - 128)) + 512;
+  x = constrain(x, 0, 1023);
+  int y = (24 * (valueArray[1] - 128)) + 512;
+  y = constrain(y, 0, 1023);
   // Serial.print(x);
   // Serial.print("\t");
   // Serial.println(y);
-  // Joystick.setXAxis(x);
-  // Joystick.setYAxis(y);
-  // Joystick.sendState();
+  Joystick.setXAxis(x);
+  Joystick.setYAxis(y);
+  Joystick.sendState();
 }
+
+// int value = Wire.read();  // receive byte as an integer
+
+// if (value != 0) {
+//   // Serial.println(value, HEX);  // print the integer
+//   int y = value & 0x0F;
+//   int x = value & 0xF0;
+//   //x = x << 4;
+//   // Joystick.setXAxis(x * 4);
+//   // Joystick.setYAxis(y * 4);
+//   // Joystick.sendState();
+//   x = x >> 4;
+//   x = x * 64;
+//   y = y * 64;
+// Serial.print(x);
+// Serial.print("\t");
+// Serial.println(y);
+// Joystick.setXAxis(x);
+// Joystick.setYAxis(y);
+// Joystick.sendState();
+//}
 
 
 // if (x == 0x01) {
@@ -162,8 +174,49 @@ void receiveEvent(int howMany) {
 //   pinMode(8, INPUT_PULLUP);
 //   digitalWrite(8, HIGH);  // turn the LED on (HIGH is the voltage level)
 // }
-}
+//}
 
+
+void test2() {
+  Joystick.setXAxis(0);
+  Joystick.setYAxis(512);
+  Joystick.sendState();
+  Serial.print("left");
+  delay(1000);
+  Joystick.setXAxis(512);
+  Joystick.setYAxis(512);
+  Joystick.sendState();
+  Serial.print("stop");
+  delay(1000);
+  Joystick.setXAxis(1023);
+  Joystick.setYAxis(512);
+  Joystick.sendState();
+  Serial.print("right");
+  delay(1000);
+  Joystick.setXAxis(512);
+  Joystick.setYAxis(512);
+  Joystick.sendState();
+  delay(1000);
+  Joystick.setXAxis(512);
+  Joystick.setYAxis(0);
+  Joystick.sendState();
+  Serial.print("down");
+  delay(1000);
+  Joystick.setXAxis(512);
+  Joystick.setYAxis(512);
+  Joystick.sendState();
+  delay(1000);
+  Joystick.setXAxis(512);
+  Joystick.setYAxis(1023);
+  Joystick.sendState();
+  Serial.print("up");
+  delay(1000);
+  Joystick.setXAxis(512);
+  Joystick.setYAxis(512);
+  Joystick.sendState();
+  Serial.print("stop");
+  delay(1000);
+}
 
 void test() {
   pinMode(13, OUTPUT);
